cc391315aaad02866aa41a497a7ba2004b2a4349
Victoriablu <Victoriablu@users.noreply.github.com>
2020-09-19 15:42:36 +1000

    added east and west movements

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,10 +1,12 @@
 module ToyRobot
     class Robot
-        attr_reader :east, :north
+        DIRECTIONS = ["NORTH", "EAST", "SOUTH", "WEST"]
+        attr_reader :east, :north, :direction
 
-        def initialize(east = 0, north = 0)
+        def initialize(east = 0, north = 0, direction = "NORTH")
             @east = east
             @north = north
+            @direction = direction
         end
 
         def move_east
@@ -19,5 +21,20 @@ module ToyRobot
             @north += 1
         end
 
+        def move_south
+            @north -= 1
+        end
+
+        def move
+            send("move_#{@direction.downcase}")
+        end
+
+        def turn_left
+            @direction = DIRECTIONS[DIRECTIONS.index(@direction) - 1]
+        end
+
+        def turn_right
+            @direction = DIRECTIONS[DIRECTIONS.index(@direction) + 1]
+        end
     end 
 end
